d464b10bd445c3e708995152457afae330d602dd
Mark Humphrey <Mark.Humphrey@gmail.com>
2015-08-30 19:45:16 -0400

    Add view and template code for blog post pagination. Only dummy data for now

--- app/bp_blog/views.py
+++ app/bp_blog/views.py
@@ -9,4 +9,20 @@ bp = Blueprint(name='blog', import_name=__name__, url_prefix='/blog',
 
 @bp.route('/', methods=['GET'])
 def index():
-    return render_template(bp.name + "/index.html")
+    older_url="%spage/%d/" % (url_for("blog.index"), 2)
+    return render_template(bp.name + "/index.html", older_url=older_url)
+
+@bp.route('/page/<int:page_num>/', methods=['GET'])
+def page(page_num):
+    if page_num <= 0:
+        return redirect(url_for("blog.index"))
+    elif page_num == 2:
+        newer_url=url_for("blog.index")
+    else:
+        newer_url="%spage/%d/" % (url_for("blog.index"), page_num - 1)
+    older_url="%spage/%d/" % (url_for("blog.index"), page_num + 1)
+    return render_template(bp.name + "/index.html", newer_url=newer_url, older_url=older_url)
+
+@bp.route("/<int:year>/<string:name>/", methods=['GET'])
+def post(year, name):
+    return "Year=%d Name=%s" % (year, name)
