df5e89e2ce5e39c38775e9d2c5064d9cfcaa3304
Mark Humphrey <Mark.Humphrey@gmail.com>
2015-10-19 13:35:50 -0400

    Fix blog blueprint css reference and add blog post date formatting

--- app/bp_blog/models.py
+++ app/bp_blog/models.py
@@ -1,34 +1,23 @@
 from app import db, ma
 
+post_hashtags = db.Table('post_hashtags',
+    db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+    db.Column('hashtag_id', db.Integer, db.ForeignKey('hashtag.id'))
+)
+
+class Hashtag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(139), unique=True)
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
     uri = db.Column(db.String(180))
     title = db.Column(db.Text)
     content = db.Column(db.Text)
+    hashtags = db.relationship('Hashtag', secondary=post_hashtags,
+                               backref=db.backref('posts', lazy='joined'))
     __table_args__ = (db.UniqueConstraint('date', 'uri', name='uq_date_uri'),)
-    def __repr__(self):
-        return "<%s(id=%r, date=%r, uri=%r, title=%r, content=%r)>" % (type(self), self.id, self.date, self.uri, self.title, self.content)
-
-#    def url(self):
-        #return "%i/%s/%s" % (self.date.year, self.date.month, self.uri)
-
-    def print_methods(self):
-        """ print all the methods of this object and their doc string"""
-        print '\n* Methods *'
-        for names in dir(self):
-            attr = getattr(self,names)
-            if callable(attr):
-                print names,':',attr.__doc__
-
-    def print_attributes(self):
-        """ print all the attributes of this object and their value """
-        print '* Attributes *'
-        for names in dir(self):
-            attr = getattr(self,names)
-            if not callable(attr): #and isinstance(attr, db.Column):
-                print names,':', type(attr)
-
 
 class PostSchema(ma.ModelSchema):
     class Meta:
