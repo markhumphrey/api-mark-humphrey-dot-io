3850e28e110060125d5f62b74f77b73c8e3fa3c2
Mark Humphrey <Mark.Humphrey@gmail.com>
2015-12-13 22:43:53 -0500

    Update config file to reflect changed docker-compose name for sql container

--- app/config.py
+++ app/config.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
 # empty path for in memory db
 #SQLALCHEMY_DATABASE_URI = 'sqlite://'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:admin123@mysql/database'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:admin123@sql/database'
 # turn on query generation logging
 SQLALCHEMY_ECHO=True
 DATABASE_CONNECT_OPTIONS = {}
--- data/loader/config.py
+++ data/loader/config.py
@@ -12,7 +12,7 @@ APP_BASE_DIR = "../../"
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
 # empty path for in memory db
 # SQLALCHEMY_DATABASE_URI = 'sqlite://'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:admin123@mysql/database'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:admin123@sql/database'
 # turn on query generation logging
 SQLALCHEMY_ECHO = True
 DATABASE_CONNECT_OPTIONS = {}
